cc68fded73b20a3a9ec5081dcee663a14cbc362b
peucabral85 <periclesccabral@gmail.com>
2024-02-19 10:44:35 -0300

    update: retirando loop que nao era necessario

--- un01/semana04/05_02-PraticaArraysLoops/ExerciciosHackerRank/BrasilComP.js
+++ un01/semana04/05_02-PraticaArraysLoops/ExerciciosHackerRank/BrasilComP.js
@@ -1,16 +1,15 @@
 const palavras = ["aveia", "manha", "ave"];
-const primeiraLetra = "c";
+const primeiraLetra = "a";
 const segundaLetra = "v";
 let temPalavra = false;
 
 for (let palavra of palavras) {
-    for (let i = 0; i < 1; i++) {
-        if (palavra[i] === primeiraLetra && palavra[i + 1] === segundaLetra) {
-            temPalavra = true;
-            console.log(palavra);
-        }
+    if (palavra[0] === primeiraLetra && palavra[1] === segundaLetra) {
+        temPalavra = true;
+        console.log(palavra);
     }
 }
+
 if (!temPalavra) {
     console.log("NENHUMA");
 }
